259159a74f31b602bcc5d76b7f8c1bae3bb22a7a
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 11:54:26 +0530

    Create 3626-SmallestDivisibleDigitProductI

---
+++ 3626-SmallestDivisibleDigitProductI/3626-SmallestDivisibleDigitProductI.java
@@ -0,0 +1,22 @@
+// Last updated: 8/12/2026, 11:54:23 AM
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (true) {
+            if (digitProduct(n) % t == 0) {
+                return n;
+            }
+            n++;
+        }
+    }
+
+    private int digitProduct(int num) {
+        int product = 1;
+
+        while (num > 0) {
+            product *= (num % 10);
+            num /= 10;
+        }
+
+        return product;
+    }
+}
