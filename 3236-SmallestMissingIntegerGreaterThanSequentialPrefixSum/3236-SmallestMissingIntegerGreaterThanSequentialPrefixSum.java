bbf87339d3c261341a895fab3406f96623beb59a
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 11:55:03 +0530

    Create 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum

---
+++ 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum.java
@@ -0,0 +1,17 @@
+// Last updated: 8/12/2026, 11:55:00 AM
+class Solution {
+    public int missingInteger(int[] nums) {
+        int countSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] + 1 == nums[i]) countSum += nums[i];
+            else break;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (countSum == nums[i]) {
+                countSum++;
+            }
+        }
+        return countSum;
+    }
+}
