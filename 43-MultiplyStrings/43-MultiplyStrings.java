f8fb227575e3fc3fb8211b9a2b98e0bfae2f77b3
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 12:17:09 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.java
@@ -0,0 +1,32 @@
+// Last updated: 8/12/2026, 12:17:06 PM
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0"))
+            return "0";
+
+        int[] res = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                int sum = mul + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0))
+                sb.append(num);
+        }
+
+        return sb.toString();
+    }
+}
