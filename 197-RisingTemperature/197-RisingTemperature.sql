67382c8d271cf01c94c2fcaf425511626ea3e6c3
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 12:11:22 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,7 @@
+-- Last updated: 8/12/2026, 12:11:19 PM
+# Write your MySQL query statement below
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature > W2.temperature;
