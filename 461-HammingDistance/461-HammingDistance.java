6d34f82a0171c5426cdd2de7d3ebdf81facdac7e
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 12:06:25 +0530

    Create 461-HammingDistance

---
+++ 461-HammingDistance/461-HammingDistance.java
@@ -0,0 +1,35 @@
+// Last updated: 8/12/2026, 12:06:21 PM
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        
+        
+        if (x == y)
+            return count;
+        
+        
+        while (x > 0 || y > 0) {
+            int xBit = 0;
+            int yBit = 0;
+            
+            
+            if (x > 0) {
+                xBit = x % 2;
+                x = Math.floorDiv(x, 2);
+            }
+            
+            
+            if (y > 0) {
+                yBit = y % 2;
+                y = Math.floorDiv(y, 2);
+            }
+            
+            
+            if (xBit != yBit)
+                count++;
+        }
+        
+        
+        return count;
+    }
+}
