070a6e1de6bb61a0c99f006ac480ec60a25f5a2e
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 11:56:33 +0530

    Create 1617-StoneGameIv

---
+++ 1617-StoneGameIv/1617-StoneGameIv.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 11:56:30 AM
+class Solution {
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+
+            // If i is a losing position,
+            // every valid square move from i
+            // creates a winning position.
+            if (!dp[i]) {
+
+                for (int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+
+                // Once n becomes reachable from a losing position,
+                // the first player can force a win.
+                if (dp[n]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
