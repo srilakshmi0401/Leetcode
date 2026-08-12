be2837fce6681adad85069af8dbeb4b58b4413c9
srilakshmi0401 <srilakshmivanshika@gmail.com>
2026-08-12 12:10:40 +0530

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -0,0 +1,25 @@
+// Last updated: 8/12/2026, 12:10:37 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next =prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    
+    }
+}
